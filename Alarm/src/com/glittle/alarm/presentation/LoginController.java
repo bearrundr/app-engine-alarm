f07ce6e18b8ab9e29605375a66121a3d980b5b12
sramanandi <sramanandi@5014d41c-e542-c299-34bf-6fcfa0d22def>
2010-05-29 16:01:29 +0000

    - Spring MVC artifacts - Login controller

--- Alarm/src/com/glittle/alarm/presentation/LoginController.java
+++ Alarm/src/com/glittle/alarm/presentation/LoginController.java
@@ -2,16 +2,46 @@ package com.glittle.alarm.presentation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.glittle.alarm.infrastructure.persistence.jpa.UserDao;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 @Controller
+@RequestMapping(value="/login/")
 public class LoginController {
-	private UserDao userDao;
 	
+	private static final String LOGIN_PAGE = "login";
+	
+	private UserDao userDao;
+	private UserService userService = UserServiceFactory.getUserService();
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String login(Model model) {
+		model.addAttribute("loginUrl", userService.createLoginURL("/app/login/success"));
+		return LOGIN_PAGE;
+	}
+	
+	@RequestMapping(value="/success", method=RequestMethod.GET)
+	public String loginSuccess() {		
+		User user = userService.getCurrentUser();		
+		if(user != null) {
+			
+		}
+		
+		return null;
+	}
 	
 	@Autowired
 	public void setUserDao(UserDao dao) {
 		this.userDao = dao;
+	}
+	
+	private User toUser(User user) {		
+		return null;
 	}
 }
